8623d5d17d4ecafdd51aafa5dc6dba0cf04b4e45
hrjy6278 <hrjy6278@naver.com>
2023-08-24 10:52:36 +0900

    Initial Commit

---
+++ CleanArchitecture-SwiftVersion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanArchitecture-SwiftVersion
+//
+//  Created by 김재윤 on 2023/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CleanArchitecture-SwiftVersionUITests/CleanArchitecture_SwiftVersionUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CleanArchitecture_SwiftVersionUITestsLaunchTests.swift
+//  CleanArchitecture-SwiftVersionUITests
+//
+//  Created by 김재윤 on 2023/08/24.
+//
+
+import XCTest
+
+final class CleanArchitecture_SwiftVersionUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
